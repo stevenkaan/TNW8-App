1cf9172727c77aab7ca1ee6d7b1fd5f6cf79aaca
Steven Kaan <stevenkaan@gmail.com>
2015-10-07 16:03:11 +0200

    added search in listview

--- app/src/main/java/nl/sightguide/sightguide/MainActivity.java
+++ app/src/main/java/nl/sightguide/sightguide/MainActivity.java
@@ -1,56 +1,48 @@
 package nl.sightguide.sightguide;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.ArrayAdapter;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    public final static String EXTRA_MESSAGE = "com.stevenkaan.sightguide.MESSAGE";
     private String[] monthsArray = { "Amsterdam", "Groningen", "Leeuwarden", "Steenwijk", "Hoogeveen", "Emmen", "Zwolle", "Utrecht", "Maastricht", "Alkmaar", "Arnhem" };
-
-    private ListView monthsListView;
-    private ArrayAdapter arrayAdapter;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        monthsListView = (ListView) findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, monthsArray);
-        monthsListView.setAdapter(arrayAdapter);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+        EditText inputSearch = (EditText) findViewById(R.id.itemSearch);
+        ListView monthsListView = (ListView) findViewById(R.id.view_cities);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, monthsArray);
+        monthsListView.setAdapter(adapter);
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                Log.d(TAG, "Pressed search");
-                return true;
-            case R.id.action_settings:
-                Log.d(TAG, "Pressed settings");
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                MainActivity.this.adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }

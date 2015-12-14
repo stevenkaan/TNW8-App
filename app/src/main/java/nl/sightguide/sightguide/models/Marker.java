7a7a1d75f2e31110340b9e02ca7ebe4e1275ea19
Wietse de Vries <wietse.l.devries@gmail.com>
2015-12-14 11:32:49 +0100

    adding city info and images

--- app/src/main/java/nl/sightguide/sightguide/models/Marker.java
+++ app/src/main/java/nl/sightguide/sightguide/models/Marker.java
@@ -20,7 +20,6 @@ public class Marker extends RealmObject {
     private int type_id;
 
 
-
     public void setId(int id) { this.id = id; }
     public void setCity(City city) { this.city = city; }
     public void setType_id(int type_id) { this.type_id = type_id; }
